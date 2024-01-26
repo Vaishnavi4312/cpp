8256feb0c7534ee6438a041cbd93260db43a820f
Vaishnavi4312 <156165700+Vaishnavi4312@users.noreply.github.com>
2024-01-26 22:00:21 +0530

    Update MyComplex.hpp

--- complex_no/MyComplex.hpp
+++ complex_no/MyComplex.hpp
@@ -10,8 +10,16 @@ public:
 
     // Function to print complex number
     void print_add();
-    void print_sub(); 
+    void print_sub();
+    void print_multi();
+    void print_div();
+    void print_norm();
+    void print_compexConjugate();
 
     void add(MyComplex, MyComplex);
     void sub(MyComplex, MyComplex);
+    void multi(MyComplex, MyComplex);
+    void div(MyComplex, MyComplex);
+    void norm(MyComplex, MyComplex);
+    void compexConjugate(MyComplex, MyComplex);
 };
